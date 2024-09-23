8a8e0009e3fad9981eaa41954ab97ccc2b6519df
Guilherme Bellini <guilhermebellinigb@hotmail.com>
2024-09-23 18:03:03 -0300

    add: tela de CadastroPedido.form.form.form

---
+++ src/main/java/br/devbellini/view/usuario/CadastroPedido.java
@@ -0,0 +1,9 @@
+package br.devbellini.view.usuario;
+
+import javax.swing.*;
+
+public class CadastroPedido {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTable table1;
+}
