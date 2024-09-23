ca7229d7025be0b8fde5b4f7eaba2d4da7bc5b8c
Guilherme Bellini <guilhermebellinigb@hotmail.com>
2024-09-23 17:51:43 -0300

    add: tela de CadastroCliente.form.form

---
+++ src/main/java/br/devbellini/view/usuario/CadastroCliente.java
@@ -0,0 +1,10 @@
+package br.devbellini.view.usuario;
+
+import javax.swing.*;
+
+public class CadastroCliente {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+}
