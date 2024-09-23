032afc8443937dc4e2a243d06cf83e5498aff4d8
Guilherme Bellini <guilhermebellinigb@hotmail.com>
2024-09-23 17:42:15 -0300

    add: tela de Registro.form

---
+++ src/main/java/br/devbellini/view/usuario/Registro.java
@@ -0,0 +1,28 @@
+package br.devbellini.view.usuario;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Registro extends JDialog {
+    private JTextField campoNome;
+    private JTextField campoEmail;
+    private JTextField campoLogin;
+    private JPasswordField campoSenha;
+    private JTextField campoTelefone;
+    private JTextField campoLoginConfirma;
+    private JPanel registroPanel;
+
+    public Registro(JFrame parent) {
+        super(parent);
+        setTitle("Registro");
+        setContentPane(registroPanel);
+        setMinimumSize(new Dimension(600,600));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        Registro registro = new Registro(null);
+    }
+}
