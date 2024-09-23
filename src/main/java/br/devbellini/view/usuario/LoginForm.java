35ebb7547b0ee135151fd9f783657cc2a8eb9df3
Guilherme Bellini <guilhermebellinigb@hotmail.com>
2024-09-23 17:02:56 -0300

    feat: correção tela de LoginForm

---
+++ src/main/java/br/devbellini/view/usuario/LoginForm.java
@@ -0,0 +1,40 @@
+package br.devbellini.view.usuario;
+
+import br.devbellini.application.interfaces.IUsuarioService;
+import br.devbellini.application.service.UsuarioService;
+import br.devbellini.domain.repository.UsuarioRepository;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginForm extends JDialog{
+
+    private final IUsuarioService _usuarioService = new UsuarioService(new UsuarioRepository());
+
+    private JTextField campoUsuario;
+    private JPasswordField campoSenha;
+    private JButton btnCancelar;
+    private JButton btnOK;
+    private JPanel JPanel;
+    private JLabel senha;
+    private JLabel usuario;
+    private JPanel loginPanel;
+
+
+    public LoginForm(JFrame parent) {
+        super(parent);
+        setTitle("Login");
+        setContentPane(loginPanel);
+        setMinimumSize(new Dimension(600,600));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+
+    public static void main(String[] args) {
+        LoginForm loginForm = new LoginForm(null);
+    }
+}
