1c25cd46da1c64096978c91ce4cc48d63fdd4445
Guilherme Bellini <guilhermebellinigb@hotmail.com>
2024-10-22 14:43:45 -0300

    add: ConsultaPedido form

--- src/main/java/br/devbellini/view/ConsultaPedido.java
+++ src/main/java/br/devbellini/view/ConsultaPedido.java
@@ -1,7 +1,36 @@
 package br.devbellini.view;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class ConsultaPedido {
-    private JTextField textField1;
+public class ConsultaPedido extends  JDialog{
+    private JTextField campoCliente;
+    private JList itensPedido;
+    private JButton btnEditar;
+    private JTextField campoValor;
+    private JButton btnPesquisar;
+    private JLabel iconConsulta;
+    private JPanel consultaPed;
+    private JButton btnAdd;
+    private JButton btnRmv;
+    private JButton btnSalvar;
+    private JLabel iconPedido;
+    private JTextField numeroPedido;
+    private JButton btnSave;
+
+    public ConsultaPedido(JFrame parent) {
+        super(parent);
+        setTitle("Consulta de Pedido");
+        setContentPane(consultaPed);
+        setMinimumSize(new Dimension(700, 600));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        ConsultaPedido consultaPedido = new ConsultaPedido(null);
+    }
 }
+
